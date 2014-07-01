e32ad71be2bbbdc87474bf7dc01bdfe92ff25f11
daddyderek <daddyderekahn@gmail.com>
2014-06-30 18:31:49 -1000

    removed the jquery slider and got the system set up

--- js/xhr.js
+++ js/xhr.js
@@ -1,22 +1,13 @@
-$(function(){
-  
-  var links = new Array();
-  $("#slideshow > a.slideimage_link").each(function(){
-    links.push($(this).attr("href"));
-  });
-  
-  var captions = new Array();
-  $("#feature .caption").each(function(){
-    captions.push($(this).html());
-  });
-  
-  $('#slideshow').crossSlide({
-      sleep: 8,
-      fade: 1
-    }, 
-    $('#slideshow a.slideimage_link img'), function(idx, img, idxOut, imgOut) {
-      $(img).wrap("<a href='"+links[idx]+"' />")
-      $("#slideshow_caption").html(captions[idx]);
-  	}
-	);
-});
+$(function() {
+
+	$().slick({
+		infinite			: true,
+		speed 				: 500,
+		fade					: true,
+		autoplay 			: true,
+		autoplaySpeed : 3000,
+		slide 				: '> div',
+		cssEase				: 'linear' 
+	});
+
+}); // $(function)
