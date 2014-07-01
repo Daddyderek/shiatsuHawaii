3fc525708f6d6dbe42f3fa03005634f6a7a0d9f9
daddyderek <daddyderekahn@gmail.com>
2014-07-01 11:09:02 -1000

    got the slick carousel working, data binding content next

--- js/xhr.js
+++ js/xhr.js
@@ -1,13 +1,11 @@
 $(function() {
-
-	$().slick({
+	$('.slide').slick({
 		infinite			: true,
 		speed 				: 500,
 		fade					: true,
+		swipe					: true,
 		autoplay 			: true,
-		autoplaySpeed : 3000,
-		slide 				: '> div',
-		cssEase				: 'linear' 
+		autoplaySpeed : 3000
 	});
 
 }); // $(function)
