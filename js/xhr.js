744204ef139ada2aa18b29938fd5d038f9300853
daddyderek <daddyderekahn@gmail.com>
2014-07-01 15:38:31 -1000

    slider is responsive, just need to work on the styling

--- js/xhr.js
+++ js/xhr.js
@@ -1,11 +1,13 @@
 $(function() {
+
 	$('.slide').slick({
 		infinite			: true,
 		speed 				: 500,
+		arrows				: false,
 		fade					: true,
 		swipe					: true,
 		autoplay 			: true,
-		autoplaySpeed : 3000
+		autoplaySpeed : 5000
 	});
 
 }); // $(function)
